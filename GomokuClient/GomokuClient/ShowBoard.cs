159b4f2730f68784d7d90a3c3ace0106f9d8b5dd
Allen Yang <allen1759@gmail.com>
2016-03-19 23:31:15 +0800

    try to print numbers on the blacks and whites

--- GomokuClient/GomokuClient/ShowBoard.cs
+++ GomokuClient/GomokuClient/ShowBoard.cs
@@ -17,10 +17,16 @@ namespace Gomoku
     {
         Graphics g;
         Image imageBlack, imageWhite, imageBoard;
+        Font ArialFont = new System.Drawing.Font("Arial", 16);
+        StringFormat drawFormat = new StringFormat();
+        SolidBrush drawBrushBlack = new SolidBrush(Color.Black);
+        SolidBrush drawBrushWhite = new SolidBrush(Color.White);
 
         public int[,] map;
+        public int[,] step;
         int boardX = 28, boardY = 28, boardLength = 480;
         int distance = 32, startx = 15, starty = 15;
+        int numDistx = 10, numDisty = 16;
 
         public delegate void myTurnHandler(String str);
         public myTurnHandler mth;
@@ -37,19 +43,33 @@ namespace Gomoku
 
             mth = new myTurnHandler(AddMessage);
 
-            //map = new int[15, 15];
-            //for (int i = 0; i < 15; i += 1)
-            //    for (int j = 0; j < 15; j += 1)
-            //        map[i, j] = 0;
-            //int[] xs = new int[] { 8, 7, 6, 5, 6, 8, 5, 9, 4, 7 };
-            //int[] ys = new int[] { 0, 1, 1, 1, 2, 3, 3, 4, 4, 5 };
-            //for (int i = 0; i < 10; i += 1)
-            //    map[xs[i], ys[i]] = 1;
-            //xs = new int[] { 4, 7, 9, 8, 7, 6, 5, 10, 3 };
-            //ys = new int[] { 1, 2, 3, 4, 4, 4, 4, 5, 5 };
-            //for (int i = 0; i < 9; i += 1)
-            //    map[xs[i], ys[i]] = 2;
-            //UpdateBoard();
+            // for test
+            step = new int[15, 15];
+            for (int i = 0; i < 15; i += 1)
+                for (int j = 0; j < 15; j += 1)
+                    step[i, j] = 0;
+            map = new int[15, 15];
+            for (int i = 0; i < 15; i += 1)
+                for (int j = 0; j < 15; j += 1)
+                    map[i, j] = 0;
+            int[] xs = new int[] { 8, 7, 6, 5, 6, 8, 5, 9, 4, 7 };
+            int[] ys = new int[] { 0, 1, 1, 1, 2, 3, 3, 4, 4, 5 };
+            int cnt = 0;
+            for (int i = 0; i < 10; i += 1)
+            {
+                map[xs[i], ys[i]] = CommandWords.BLACK;
+                step[xs[i], ys[i]] = ++cnt;
+            }
+            
+            xs = new int[] { 4, 7, 9, 8, 7, 6, 5, 10, 3 };
+            ys = new int[] { 1, 2, 3, 4, 4, 4, 4, 5, 5 };
+            for (int i = 0; i < 9; i += 1)
+            {
+                map[xs[i], ys[i]] = CommandWords.WHITE;
+                step[xs[i], ys[i]] = ++cnt;
+            }
+
+            UpdateBoard();
             //Black: 8 B
             //White: 8 E
             //Black: 7 D
@@ -77,21 +97,27 @@ namespace Gomoku
             {
                 for (int j = 0; j < map.GetLength(1); j += 1)
                 {
-                    if( map[i,j]!=0 )
+                    if (map[i, j] != 0)
+                    {
                         UpdateBoard(i, j, map[i, j]);
+                    }
                 }
             }
         }
 
         public void UpdateBoard(int i, int j, int whichSide)
         {
-            if( whichSide==1)
+            if( whichSide==CommandWords.BLACK )
             {
                 g.DrawImage(imageBlack, startx + j * distance, starty + i * distance, 64, 64);
+                string drawString = step[i, j].ToString("00");
+                g.DrawString(drawString, ArialFont, drawBrushWhite, startx + numDistx + j * distance, starty + numDisty + i * distance, drawFormat);
             }
-            else if( whichSide==2 )
+            else if( whichSide==CommandWords.WHITE )
             {
                 g.DrawImage(imageWhite, startx + j * distance, starty + i * distance, 64, 64);
+                string drawString = step[i, j].ToString("00");
+                g.DrawString(drawString, ArialFont, drawBrushBlack, startx + numDistx + j * distance, starty + numDisty + i * distance, drawFormat);
             }
             else
             {
